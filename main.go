507d3b16ad9e05a38f9eb74e536cdcdeb428f387
Jakob Borg <jakob@nym.se>
2015-08-11 16:48:28 +0200

    SHA256 vs SHAKE256

--- main.go
+++ main.go
@@ -11,20 +11,19 @@ import (
 	"crypto/rand"
 
 	"crypto/sha256"
-	"crypto/sha512"
 
-	"github.com/AudriusButkevicius/gohashcompare/crypto/blake2b"
-	"github.com/AudriusButkevicius/gohashcompare/crypto/blake2s"
-	"github.com/AudriusButkevicius/gohashcompare/crypto/skein"
-
-	"github.com/AudriusButkevicius/gohashcompare/crypto/blake2bmodified"
-	"github.com/AudriusButkevicius/gohashcompare/crypto/blake2smodified"
-
-	blake2cgo "github.com/AudriusButkevicius/blake2b-opt"
+	"golang.org/x/crypto/sha3"
 )
 
-var hashvalue = make([]byte, 64)
+var hashvalue []byte
 var blocksize int64 = 1 << 17
+var buf = make([]byte, blocksize)
+
+func init() {
+	if _, err := rand.Read(buf); err != nil {
+		panic(err)
+	}
+}
 
 func must(hash hash.Hash, err error) hash.Hash {
 	if err != nil {
@@ -35,25 +34,18 @@ func must(hash hash.Hash, err error) hash.Hash {
 
 func wrap(hasher hash.Hash) func(*testing.B) {
 	return func(b *testing.B) {
-		buf := make([]byte, blocksize)
-		rand.Read(buf)
 		b.SetBytes(blocksize)
-		b.StopTimer()
-		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
-			rand.Read(buf)
-			b.StartTimer()
 			hasher.Write(buf)
 			hashvalue = hasher.Sum(nil)
 			hasher.Reset()
-			b.StopTimer()
 		}
 	}
 }
 
 func main() {
 	if len(os.Args) == 1 {
-		cmd := exec.Command(os.Args[0], "-test.bench=.*", "-test.benchmem=true")
+		cmd := exec.Command(os.Args[0], "-test.bench=.*", "-test.benchmem=true", "-test.benchtime=2s")
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 		cmd.Run()
@@ -62,31 +54,40 @@ func main() {
 
 	fmt.Printf("Build: %s %s-%s\n", runtime.Version(), runtime.GOOS, runtime.GOARCH)
 
-	runtime.GOMAXPROCS(runtime.NumCPU())
-
-	hashers := map[string]hash.Hash{
-		"SHA256":               sha256.New(),
-		"SHA512":               sha512.New(),
-		"Blake2b 256":          blake2b.New256(),
-		"Blake2b 512":          blake2b.New512(),
-		"Blake2s 256":          blake2s.New256(),
-		"Blake2b 256 Modified": blake2bmodified.New256(),
-		"Blake2b 512 Modified": blake2bmodified.New512(),
-		"Blake2s 256 Modified": blake2smodified.New256(),
-		"Skein 256":            must(skein.New(skein.Skein256, 1<<8)),
-		"Skein 512":            must(skein.New(skein.Skein512, 1<<8)),
-		"Skein 1024":           must(skein.New(skein.Skein1024, 1<<8)),
-		"Blake2b CGO 512":      blake2cgo.NewBlake2B(),
+	hashers := []struct {
+		name string
+		hash hash.Hash
+	}{
+		{"SHA256", sha256.New()},
+		{"SHAKE256", shakeAdapter{sha3.NewShake256(), 64}},
 	}
 
 	benchmarks := []testing.InternalBenchmark{}
-	for name, hash := range hashers {
+	for _, h := range hashers {
 		benchmarks = append(benchmarks, testing.InternalBenchmark{
-			Name: name,
-			F:    wrap(hash),
+			Name: h.name,
+			F:    wrap(h.hash),
 		})
 	}
 	testing.Main(func(pat, str string) (bool, error) {
 		return true, nil
 	}, nil, benchmarks, nil)
+}
+
+type shakeAdapter struct {
+	sha3.ShakeHash
+	length int
+}
+
+func (a shakeAdapter) Sum(_ []byte) []byte {
+	bs := make([]byte, a.length)
+	a.Read(bs)
+	return bs
+}
+
+func (a shakeAdapter) Size() int {
+	return a.length
+}
+func (a shakeAdapter) BlockSize() int {
+	return 1
 }
